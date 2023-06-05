75201a09faee640bb3fb6882686d6cbeb9457c72
DevRenato <u18215194@gmail.com>
2023-06-05 17:23:30 -0500

    seguridad de las paginas y icono agregado en el head

--- app/views/layouts/ProjectLayout.php
+++ app/views/layouts/ProjectLayout.php
@@ -8,6 +8,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet"> 
   <link rel="stylesheet" href="/build/css/app.css">
+
+  <link rel="icon" href="/icons/favicon.svg">
 </head>
 <body>
 
