4f6afcb6532decb47789695c4df71491311cea01
DevRenato <u18215194@gmail.com>
2023-06-05 01:08:38 -0500

    actuqalizando js

--- src/js/task/store/taskStore.js
+++ src/js/task/store/taskStore.js
@@ -2,16 +2,64 @@ import { loadTaskByProject } from "../useCases/loadTaskByProject";
 
 const state = {
   tasks: [],
-  selectedTask: null,
+  currentTask: 0,
+};
+
+const loadNextPage = async (projectId) => {
+  const tasks = await loadTaskByProject(projectId, state.currentTask + 1);
+  if (tasks.length === 0) return;
+
+  state.currentTask++;
+  state.tasks = tasks;
+};
+
+const loadPreviusPage = async (projectId) => {
+  if (state.currentTask <= 1) return;
+
+  const tasks = await loadTaskByProject(projectId, state.currentTask - 1);
+
+  state.currentTask--;
+  state.tasks = tasks;
+};
+
+const onChangeTask = async (taskUpdated) => {
+  let wasFound = false;
+
+  state.tasks = state.tasks.map((task) => {
+    if (task.id === taskUpdated.id) {
+      wasFound = true;
+      return taskUpdated;
+    }
+
+    return task;
+  });
+
+  // Si no se encontró la tarea, se agrega
+  if (!wasFound) {
+    state.tasks.push(taskUpdated);
+  }
+};
+
+const setFilter = (status) => {
+  state.filter = status;
 };
 
 const reloadPage = async (projectId) => {
   const tasks = await loadTaskByProject(projectId);
+  if (tasks.length === 0) {
+    await loadPreviusPage(projectId);
+    return;
+  }
 
   state.tasks = tasks;
 };
 
 export default {
+  loadNextPage,
+  loadPreviusPage,
   reloadPage,
+  onChangeTask,
+
   getTasks: () => [...state.tasks],
+  getCurrentPage: () => state.currentTask,
 };
