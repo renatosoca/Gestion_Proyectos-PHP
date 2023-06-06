d8cff940f2a6e09789651071db4d020eb8841c1f
DevRenato <u18215194@gmail.com>
2023-06-06 03:10:38 -0500

    sidebar finalizado

--- app/views/projects/profile.php
+++ app/views/projects/profile.php
@@ -1,31 +1,47 @@
-<div class="container-sm">
-  <?php include_once __DIR__.'/../template/alerts.php'; ?>
+<div class="project__container profile">
+  <div class="profile__container">
+    <?php include_once __DIR__.'/../template/alerts.php'; ?>
+  
+    <a href="/user/change-password" class="btn">Cambiar Contraseña</a>
+  
+    <form action="/user/profile" class="form" method="POST">
+      <div class="form__field">
+        <label for="name" class="form__field--label">Nombre</label>
+        <input 
+          class="form__field--input"
+          type="text" 
+          name="name" 
+          id="name" 
+          value="<?php echo sanitize($user->name ?? ''); ?>" 
+          placeholder="Tu bombre"
+        >
+      </div>
 
-  <a href="/user/change-password" class="btn">Cambiar Contraseña</a>
-
-  <form action="/user/profile" class="formulario" method="POST">
-    <div class="campo">
-      <label for="name">Nombre</label>
-      <input 
-        type="text" 
-        name="name" 
-        id="name" 
-        value="<?php echo sanitize($user->name ?? ''); ?>" 
-        placeholder="Tu bombre"
-      >
-    </div>
-    
-    <div class="campo">
-      <label for="email">Email</label>
-      <input 
-        type="email" 
-        name="email" 
-        id="email" 
-        value="<?php echo sanitize($user->email ?? ''); ?>" 
-        placeholder="Tu Email"
-      >
-    </div>
-
-    <input type="submit" value="Guardar Cambio" class="btn">
-  </form>
+      <div class="form__field">
+        <label for="lastname" class="form__field--label">Apellido</label>
+        <input 
+          class="form__field--input"
+          type="text" 
+          name="lastname" 
+          id="lastname" 
+          value="<?php echo sanitize($user->lastname ?? ''); ?>" 
+          placeholder="Tu bombre"
+        >
+      </div>
+      
+      <div class="form__field">
+        <label for="email" class="form__field--label">Email</label>
+        <input 
+          class="form__field--input"
+          type="email" 
+          name="email" 
+          id="email" 
+          value="<?php echo sanitize($user->email ?? ''); ?>" 
+          placeholder="Tu Email"
+        >
+      </div>
+  
+      <input type="submit" value="Guardar Cambio" class="form__submit">
+    </form>
+  </div>
 </div>
--- app/views/template/sidebar.php
+++ app/views/template/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="layout__project--sidebar sidebar">
   <nav class="sidebar__nav">
-    <a class="sidebar__link <?php echo isLinkActive('/dashboard') ? 'activo' : ''; ?>" href="/dashboard">
+    <a class="sidebar__link <?php echo isLinkActive('/dashboard') ? 'active' : ''; ?>" href="/dashboard">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="sidebar__link--icon">
         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
       </svg>
@@ -8,7 +8,7 @@
       Proyectos
     </a>
     
-    <a class="sidebar__link <?php echo isLinkActive('/create-project') ? 'activo' : ''; ?>" href="/create-project">
+    <a class="sidebar__link <?php echo isLinkActive('/create-project') ? 'active' : ''; ?>" href="/create-project">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="sidebar__link--icon">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
       </svg>
@@ -16,7 +16,7 @@
       Crear Proyecto
     </a>
 
-    <a class="sidebar__link <?php echo isLinkActive('/user/profile') ? 'activo' : ''; ?>" href="/user/profile">
+    <a class="sidebar__link <?php echo isLinkActive('/user/profile') ? 'active' : ''; ?>" href="/user/profile">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="sidebar__link--icon">
         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
       </svg>
@@ -24,4 +24,14 @@
       Perfil
     </a>
   </nav>
+
+  <div class="sidebar__menu">
+    <a href="/logout" class="sidebar__link sidebar__link-logout">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="sidebar__link--icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+      </svg>
+
+      Salir
+    </a>
+  </div>
 </aside>
